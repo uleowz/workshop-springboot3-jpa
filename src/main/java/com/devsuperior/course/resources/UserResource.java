856962a598b3c7f55e9aad79aa3b85ac823e1901
Leonardo Azzola <uleowz@gmail.com>
2023-07-26 18:16:29 -0300

    User entity and resource

---
+++ src/main/java/com/devsuperior/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.devsuperior.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.course.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Fulano", "fulano@gmail.com", "99 99999999","123321");
+		return ResponseEntity.ok().body(u);
+	}
+	
+}
