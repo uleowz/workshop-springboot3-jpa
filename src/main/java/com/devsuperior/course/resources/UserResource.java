41038864d3482275b6adb08f87dd3c40c7c68a3b
Leonardo Azzola <uleowz@gmail.com>
2023-09-30 23:29:08 -0300

    Service layer, component registration

--- src/main/java/com/devsuperior/course/resources/UserResource.java
+++ src/main/java/com/devsuperior/course/resources/UserResource.java
@@ -1,20 +1,33 @@
 package com.devsuperior.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.course.entities.User;
+import com.devsuperior.course.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Fulano", "fulano@gmail.com", "99 99999999","123321");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = userService.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping(value="/{id}") // Para falar que a URL tera um parametro.
+	public ResponseEntity<User> findById(@PathVariable Long id){ //para dizer que o parametro do método virá do parametro da url
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
